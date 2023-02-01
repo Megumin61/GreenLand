703bb8160bd90eaa0f7ab886f7e7ffcfad1c347e
张杰斯 <790201678@qq.com>
2023-02-01 13:26:21 +0800

    2023/2/1 完成数据库架构，viewmodel增删改查测试

--- app/src/main/java/com/example/jetpacktest02/RallyActivity.kt
+++ app/src/main/java/com/example/jetpacktest02/RallyActivity.kt
@@ -3,23 +3,29 @@ package com.example.jetpacktest02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.jetpacktest02.Entity.User
+import com.example.jetpacktest02.ViewModel.MainViewModel
 import com.example.jetpacktest02.ViewModel.UserViewModel
 import com.example.jetpacktest02.compose.MyBottomNavBar
+import com.example.jetpacktest02.config.UsersApplication
 import com.example.jetpacktest02.ui.main.*
 import com.example.scaffolddemo.ui.theme.ScaffoldDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import java.util.Objects
 
 
 /**
@@ -29,31 +35,54 @@ import dagger.hilt.android.AndroidEntryPoint
 fun NavHostController.navigateSingleTopTo(route: String) =
     this.navigate(route) { launchSingleTop = true }
 
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class RallyActivity : ComponentActivity() {
-//    private val newUserActivityRequestCode = 1
-//    private val userViewModel: UserViewModel by viewModels {
-//        UserViewModelFactory((application as UsersApplication).repository)
-//    }
-
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RallyApp()
 
+        setContent {
+//            WordBookApp()
+//            CounterScreen()
+            RallyApp()
         }
+    }
+
+//    override fun onResume() {
+//        super.onResume()
+//        GlobalScope.launch {
+//            val user = User("Hello","12222")
+//            UsersApplication.database.userDao().insertUser(user)
+//
+//        }
+//    }
+}
+
+@Composable
+fun WordBookApp(userViewModel: UserViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+//    val count by mainViewModel.counterLiveData.observeAsState(0)
+    val users: List<User> by userViewModel.allUsers.observeAsState(mutableListOf())
+//    val users by viewModel.allUsers.observeAsState(listOf())
+    val user : User= userViewModel.getUser(2)
+
+    Column{
+        Text("success")
+        Text(user.name)
+//        Text(users[0].name)
+//        for (i :User in users)
+//            Text(i.name.toString())
+        Text(users.size.toString())
     }
 }
 
 @Composable
-fun RallyApp(viewModel: UserViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
-//    val weatherNow = viewModel.weatherNow.observeAsState()
-//    println(weatherNow.value.toString())
+fun RallyApp() {
+
+//    val users by viewModel.allUsers.observeAsState(listOf())
     var currentScreen: RallyDestination by remember { mutableStateOf(Overview) }
     val navController = rememberNavController()
     val currentBackStack by navController.currentBackStackEntryAsState()
     val Textvalue: String = ""
-
 
     // Fetch your currentDestination:
     val currentDestination = currentBackStack?.destination
--- app/src/main/java/com/example/jetpacktest02/screen/IslandDeliverScreen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jetpacktest02.screen
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun IslandDeliverScreen(){}

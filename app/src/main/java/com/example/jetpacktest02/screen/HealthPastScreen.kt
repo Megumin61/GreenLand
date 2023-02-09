f5889263726dc6024f7cd4b73aa7ebc26d55da32
张杰斯 <790201678@qq.com>
2023-02-09 22:51:05 +0800

    2023/2/9 健康总结添加viewpager局部跳转

--- app/src/main/java/com/example/jetpacktest02/screen/HealthPastScreen.kt
+++ app/src/main/java/com/example/jetpacktest02/screen/HealthPastScreen.kt
@@ -62,94 +62,38 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpacktest02.R
+import com.example.jetpacktest02.ViewModel.UserViewModel
 import com.example.jetpacktest02.compose.MyTopAppBar
+import com.example.jetpacktest02.screen.FriendList
+import com.example.jetpacktest02.screen.IconButtonFriendList
 import com.example.scaffolddemo.ui.theme.Gray1
 import com.example.scaffolddemo.ui.theme.Green1
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 /**
  * The Bills screen.
  */
-@Preview(showBackground=true,widthDp=393,heightDp=851)
+@Preview(showBackground = true, widthDp = 393, heightDp = 851)
 @Composable
 fun HealthPastScreen(
 //            bills : (String) -> Unit = {},
-    nav01: () -> Unit={},
+    nav01: () -> Unit = {},
 
-) {
+    ) {
+    //配置顶部状态栏颜色
+    rememberSystemUiController().setStatusBarColor(
+        Color.White, darkIcons = androidx.compose.material.MaterialTheme.colors.isLight
+    )
+
     Column {
         HealthTopAppBar()
-        HealthViewTabRow()
-        Box(
-            modifier = Modifier
-                .size(393.dp, 2000.dp)
-                .background(
-                    brush = Brush.verticalGradient(
-                        listOf(
-                            Color(246, 235, 222),
-                            Color(195, 216, 190)
-                        )
-                    )
-                )
-        )
-        {
-            /*Column(modifier = Modifier
-                .padding(top = 20.dp, start = 26.dp, end = 26.dp)
-                , verticalArrangement = Arrangement.spacedBy(20.dp)
-            ) {
-                StepFrame()
-                SitFrame()
-                WaterFrame()
+        PastHealthViewTabRow()
 
-            }*/
-
-Column(modifier=Modifier.padding(top=10.dp)) {
-    LazyRowPlant01()
-    LazyColumn(
-        Modifier
-            .fillMaxWidth() // 宽度填满父空间
-            .height(2000.dp)
-            .padding(top = 0.dp, start = 26.dp, end = 26.dp),
-
-
-        verticalArrangement = Arrangement.spacedBy(22.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-
-        content = {
-
-            item{
-                Row (verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(24.dp)){
-                    ImgPlant()
-                    InformationRow()
-                }
-
-
-            }
-            item{
-                StepFrame()
-            }
-            item{
-                SitFrame()
-            }
-            item{
-                WaterFrame()
-            }
-            item{
-                ImgAdviceFrame()
-            }
-            item{
-                BtnGetConclusion()
-            }
-
-
-        })
-}
-
-        }
     }
-
-
 
 
     /*Column{
@@ -173,97 +117,226 @@ Column(modifier=Modifier.padding(top=10.dp)) {
 
 
 @Composable
-fun LazyRowPlant01(){
-Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(0.dp)
-){
-    Image(
-        painter = painterResource(id = R.drawable.g6_2_rowbg),
-        contentDescription = null,
-    )
-    LazyRow(modifier =Modifier.width(320.dp),
-        horizontalArrangement = Arrangement.spacedBy(10.dp),
-        content =
-        {
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant7),
-                        contentDescription = null,
-                    )
-                    Text(text = "8.18", fontSize = 12.sp,color=Color(149,152,172))
+fun LazyRowPlant01() {
+    Box(
+        contentAlignment = Alignment.Center, modifier = Modifier.padding(0.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.g6_2_rowbg),
+            contentDescription = null,
+        )
+        LazyRow(modifier = Modifier.width(320.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            content =
+            {
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant7),
+                            contentDescription = null,
+                        )
+                        Text(text = "8.18", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant8),
-                        contentDescription = null,
-                    )
-                    Text(text = "8.25", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant8),
+                            contentDescription = null,
+                        )
+                        Text(text = "8.25", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant1),
-                        contentDescription = null,
-                    )
-                    Text(text = "9.1", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant1),
+                            contentDescription = null,
+                        )
+                        Text(text = "9.1", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant2),
-                        contentDescription = null,
-                    )
-                    Text(text = "9.8", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant2),
+                            contentDescription = null,
+                        )
+                        Text(text = "9.8", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant3),
-                        contentDescription = null,
-                    )
-                    Text(text = "9.15", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant3),
+                            contentDescription = null,
+                        )
+                        Text(text = "9.15", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant4),
-                        contentDescription = null,
-                    )
-                    Text(text = "9.22", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant4),
+                            contentDescription = null,
+                        )
+                        Text(text = "9.22", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant5),
-                        contentDescription = null,
-                    )
-                    Text(text = "9.29", fontSize = 12.sp)
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant5),
+                            contentDescription = null,
+                        )
+                        Text(text = "9.29", fontSize = 12.sp)
+                    }
                 }
-            }
-            item{
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(
-                        painter = painterResource(id = R.drawable.g6_2_plant6),
-                        contentDescription = null,
-                    )
-                    Text(text = "10.6", fontSize = 12.sp,color=Color(149,152,172))
+                item {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(
+                            painter = painterResource(id = R.drawable.g6_2_plant6),
+                            contentDescription = null,
+                        )
+                        Text(text = "10.6", fontSize = 12.sp, color = Color(149, 152, 172))
+                    }
                 }
-            }
 
 
-        }
-    )
+            }
+        )
+
+    }
+
 
 }
 
-   
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun PastHealthViewTabRow() {
+    //state为顶部的tab导航栏绑定参数
+    var state by remember { mutableStateOf(0) }
+    //pagerState为底部viewpager参数
+    val pagerState: PagerState = remember { PagerState() }
+
+    //将底部pager的参数和顶部导航栏的参数state绑定，让pager响应顶部导航栏参数变化
+    LaunchedEffect(pagerState) {
+        snapshotFlow { state }.collect { page ->
+            pagerState.animateScrollToPage(page)
+        }
+    }
+    //将底部pager的参数和顶部导航栏的参数state绑定
+    LaunchedEffect(pagerState) {
+        snapshotFlow { pagerState.currentPage }.collect { page ->
+            state = page
+        }
+    }
+    val titles = listOf("本周", "过往")
+    Column {
+        androidx.compose.material.TabRow(
+            selectedTabIndex = state,
+            indicator = @Composable { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.customTabIndicatorOffset(tabPositions[state]),
+                    color = Color(26, 207, 163)
+                )
+            }
+        ) {
+            titles.forEachIndexed { index, title ->
+                androidx.compose.material.Tab(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .width(10.dp),
+                    selected = state == index,
+                    onClick = { state = index },
+                    text = {
+                        androidx.compose.material.Text(
+                            text = title,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.W800,
+                        )
+                    },
+                    selectedContentColor = Color.Black,
+                    unselectedContentColor = Gray1
+                )
+            }
+        }
+    }
+
+    HorizontalPager(count = 2, state = pagerState) { page ->
+        Text(text = "Page: $page")
+        //下面为要滑动切换的界面，可以通过判断page调用不同页面
+        Box(
+            modifier = Modifier
+                .size(393.dp, 2000.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            Color(246, 235, 222),
+                            Color(195, 216, 190)
+                        )
+                    )
+                )
+        )
+        {
+            /*Column(modifier = Modifier
+                .padding(top = 20.dp, start = 26.dp, end = 26.dp)
+                , verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                StepFrame()
+                SitFrame()
+                WaterFrame()
+            }*/
+
+            Column(modifier = Modifier.padding(top = 10.dp)) {
+                LazyRowPlant01()
+                LazyColumn(
+                    Modifier
+                        .fillMaxWidth() // 宽度填满父空间
+                        .height(2000.dp)
+                        .padding(top = 0.dp, start = 26.dp, end = 26.dp),
+
+
+                    verticalArrangement = Arrangement.spacedBy(22.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+                    content = {
+
+                        item {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(24.dp)
+                            ) {
+                                ImgPlant()
+                                InformationRow()
+                            }
+
+
+                        }
+                        item {
+                            StepFrame()
+                        }
+                        item {
+                            SitFrame()
+                        }
+                        item {
+                            WaterFrame()
+                        }
+                        item {
+                            ImgAdviceFrame()
+                        }
+                        item {
+                            BtnGetConclusion()
+                        }
+
+
+                    })
+            }
+
+        }
+    }
 }
 
 
